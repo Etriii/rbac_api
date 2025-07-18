8d482fcc633422892499d5bf4d2341e8b9269cb3
Alex, Jr. A. Aparece <alexarnaizaparece@gmail.com>
2025-07-18 18:49:00 +0800

    feat(db): Added database connection           db url is in env

--- app/db.py
+++ app/db.py
@@ -1,1 +1,20 @@
-#
+from sqlmodel import create_engine, Session, SQLModel
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+DATABASE_URL = os.environ.get("DATABASE_URL")
+
+engine = create_engine(DATABASE_URL)
+
+def get_db():
+    with Session(engine) as session:
+        yield session
+
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
+    
+def drop_all_tables_in_db():
+        SQLModel.metadata.drop_all(engine)
